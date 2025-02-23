1ce94682498cf3eb6b003c96553446f71d908b72
GroundbreakingMC <noslowdwn@gmail.com>
2025-02-23 14:17:59 +0100

    MyLib upstream

--- src/main/java/com/github/groundbreakingmc/kidaypisun/utils/config/ConfigValues.java
+++ src/main/java/com/github/groundbreakingmc/kidaypisun/utils/config/ConfigValues.java
@@ -2,7 +2,7 @@ package com.github.groundbreakingmc.kidaypisun.utils.config;
 
 import com.github.groundbreakingmc.kidaypisun.KidayPisun;
 import com.github.groundbreakingmc.mylib.config.ConfigLoader;
-import com.github.groundbreakingmc.mylib.logger.LoggerFactory;
+import com.github.groundbreakingmc.mylib.logger.console.LoggerFactory;
 import com.google.common.collect.ImmutableMap;
 import lombok.AccessLevel;
 import lombok.Getter;
